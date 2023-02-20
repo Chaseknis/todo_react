14a1774d75d77ec89a5902258736f367b13ff28e
chaseknis <chase6th@gmail.com>
2023-02-20 12:11:58 +0200

    Update src directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [toDo, setToDo] = useState([
+    {"id": 1, "title": "Task 1", "status": false},
+    {"id": 2 , "title": "Task 2", "status": false}
+  ]);
+
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setUpdateData] = useState('');
+
+  const addTask = () => {
+
+  };
+
+  const deleteTask = (id) => {
+
+  };
+
+  const markDone = (id) => {
+
+  }; 
+
+  const cancelUpdate = () => {
+
+  };
+
+  const changeTask = (e) => {
+
+  };
+
+  const updateTask = () => {
+
+  };
+
+    // The counter
+
+  const [count, setCount] = useState(() => {
+
+    return 4
+  });
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1);
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='todoWrapper'>
+        <h2>
+          Todo List App
+        </h2>
+
+        {/* Update Task Form */}
+
+        {/* Add Task Form */}
+
+        <div className='formContainer'>
+          <div className='inputWrapper'>
+            <input/>
+          </div>
+
+          <div className='buttonWrapper'>
+            <button className='success'>
+              Add Task
+            </button>
+          </div>
+        </div>
+
+        {/* Display Tasks */}
+
+        <div className='listContainer'>
+          {toDo && toDo.length ? '' : 'No tasks...'}
+          {toDo && toDo 
+            .sort((a, b) => a.id > b.id ? 1 : -1)
+            .map((task, index) => {
+              return(
+                <React.Fragment key={task.id}>
+                  <div className='tasksWrapper'>
+                    <div className={task.status ? 'done' : ''}>
+                      <span className='taskNumber'>{index + 1}</span>
+                      <span className='taskText'>{task.title}</span>
+                    </div>
+
+                    <div className='iconsWrapper'>
+                      <span title='Completed / Not completed'>
+                        <FontAwesomeIcon icon={faCircleCheck}/>
+                      </span>
+                      
+                      <span title='Edit'>
+                        <FontAwesomeIcon icon={faPen}/>
+                      </span>
+
+                      <span title='Delete'>
+                        <FontAwesomeIcon icon={faTrashCan}/>
+                      </span>
+                    </div>
+
+                  </div>
+                </React.Fragment>  
+              )
+            })
+          }
+        </div>
+
+      </div>
+
+
+      <div className='counterWrapper'>
+        <button onClick={decrementCount}>-</button>
+        <span>{count}</span>
+        <button onClick={incrementCount}>+</button>
+      </div>
     </div>
   );
 }
