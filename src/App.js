4076e37347ae1180a11b0cd54d6ae8bcc8a04da8
chaseknis <chase6th@gmail.com>
2023-02-20 13:45:32 +0200

    Add linters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faCircleCheck, faPen, faTrashCan
+  faCircleCheck, faPen, faTrashCan,
 } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [toDo, setToDo] = useState([
-    {"id": 1, "title": "Task 1", "status": false},
-    {"id": 2 , "title": "Task 2", "status": false}
+    { id: 1, title: 'Task 1', status: false },
+    { id: 2, title: 'Task 2', status: false },
   ]);
 
   const [newTask, setNewTask] = useState('');
@@ -24,7 +24,7 @@ function App() {
 
   const markDone = (id) => {
 
-  }; 
+  };
 
   const cancelUpdate = () => {
 
@@ -38,25 +38,21 @@ function App() {
 
   };
 
-    // The counter
+  // The counter
 
-  const [count, setCount] = useState(() => {
-
-    return 4
-  });
+  const [count, setCount] = useState(() => 4);
 
   function decrementCount() {
-    setCount(prevCount => prevCount - 1);
+    setCount((prevCount) => prevCount - 1);
   }
 
   function incrementCount() {
-    setCount(prevCount => prevCount + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
-
   return (
-    <div className='container'>
-      <div className='todoWrapper'>
+    <div className="container">
+      <div className="todoWrapper">
         <h2>
           Todo List App
         </h2>
@@ -65,13 +61,13 @@ function App() {
 
         {/* Add Task Form */}
 
-        <div className='formContainer'>
-          <div className='inputWrapper'>
-            <input/>
+        <div className="formContainer">
+          <div className="inputWrapper">
+            <input />
           </div>
 
-          <div className='buttonWrapper'>
-            <button className='success'>
+          <div className="buttonWrapper">
+            <button className="success">
               Add Task
             </button>
           </div>
@@ -79,44 +75,40 @@ function App() {
 
         {/* Display Tasks */}
 
-        <div className='listContainer'>
+        <div className="listContainer">
           {toDo && toDo.length ? '' : 'No tasks...'}
-          {toDo && toDo 
-            .sort((a, b) => a.id > b.id ? 1 : -1)
-            .map((task, index) => {
-              return(
-                <React.Fragment key={task.id}>
-                  <div className='tasksWrapper'>
-                    <div className={task.status ? 'done' : ''}>
-                      <span className='taskNumber'>{index + 1}</span>
-                      <span className='taskText'>{task.title}</span>
-                    </div>
-
-                    <div className='iconsWrapper'>
-                      <span title='Completed / Not completed'>
-                        <FontAwesomeIcon icon={faCircleCheck}/>
-                      </span>
-                      
-                      <span title='Edit'>
-                        <FontAwesomeIcon icon={faPen}/>
-                      </span>
-
-                      <span title='Delete'>
-                        <FontAwesomeIcon icon={faTrashCan}/>
-                      </span>
-                    </div>
-
+          {toDo && toDo
+            .sort((a, b) => (a.id > b.id ? 1 : -1))
+            .map((task, index) => (
+              <React.Fragment key={task.id}>
+                <div className="tasksWrapper">
+                  <div className={task.status ? 'done' : ''}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
                   </div>
-                </React.Fragment>  
-              )
-            })
-          }
+
+                  <div className="iconsWrapper">
+                    <span title="Completed / Not completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
+
+                </div>
+              </React.Fragment>
+            ))}
         </div>
 
       </div>
 
-
-      <div className='counterWrapper'>
+      <div className="counterWrapper">
         <button onClick={decrementCount}>-</button>
         <span>{count}</span>
         <button onClick={incrementCount}>+</button>
